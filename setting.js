3a7800505fda59f1bcfc2155279179b64ab3e3ae
KING-HANSA <hansadewmina2008@gmail.com>
2025-03-15 11:12:04 +0530

    ENCODE ALL AT 2025 march 15

---
+++ setting.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "vAAmAa5I#GxJ9DhLEWPAuBeSkhRevaV-1HIhEmZgzshIIa6164FY",
+MONGODB: process.env.MONGODB || "mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama",
+LANG: process.env.LANG || "SI"
+};
+
+
